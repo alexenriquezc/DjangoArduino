cfdac5bb7e3c3d6aaa273ece7d721423a11c466e
Alex Enríquez <djalex129@gmail.com>
2014-10-22 19:18:46 -0500

    validando que haya conexión entre arduino y el server de django

--- app/views.py
+++ app/views.py
@@ -2,22 +2,25 @@ from django.http import HttpResponse
 import urllib2
 import json
 import serial
-conexion=serial.Serial("COM3",9600)
+try:
+    conexion=serial.Serial("COM3",9600)
+except:
+    pass
 def home(request):
 
-    if conexion.isOpen():
-        return HttpResponse("Lacomunicacion se ha establecido")
-    else:
-        return HttpResponse("No hay conexion")
+    return HttpResponse("<style>h1{color:blue}</style><h1>Djangoduino</h1>")
 
 def encender(request,estado):
-    if estado=="1":
-        conexion.write("1")
-        return HttpResponse("<h1>Led encendido</h1><script>console.log(%s)</script>"%estado)
+    try:
+        if estado=="1":
+            conexion.write("1")
+            return HttpResponse("<h1>Led encendido</h1><script>console.log(%s)</script>"%estado)
 
-    elif estado>"1":
-        return HttpResponse("<h1>En estos momentos no esta disponible</h1>")
+        elif estado>"1":
+            return HttpResponse("<h1>En estos momentos no esta disponible</h1>")
 
-    else:
-        conexion.write("0")
-        return HttpResponse("<h1>Led apagado</h1><script>console.log(%s)</script>"%estado)
+        else:
+            conexion.write("0")
+            return HttpResponse("<h1>Led apagado</h1><script>console.log(%s)</script>"%estado)
+    except:
+        return HttpResponse("El Arduino no se encuentra conectado")
